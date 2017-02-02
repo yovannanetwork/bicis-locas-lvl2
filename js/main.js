817aa381c8d72715026d2443d919d21b6fe4064b
yovannanetwork <yovannanetwork@gmail.com>
2017-02-02 09:40:10 -0600

    oll ok

--- js/main.js
+++ js/main.js
@@ -1,88 +1,110 @@
-// validacion del campo nombre y apellido para que la primera letra sea mayuscula.
-function alerta(space, valor ){
+function validateForm(){
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var opciones = document.getElementById("opciones").value;
+	var mensaje = document.getElementById("mensaje").value;
+	
+	if(nombre.length == 0 || apellido.length==0 || correo.length==0 || password.length==0 || opciones ==0){
+		mensaje();
+		validacion1();
+		validacionApellido();
+		validateCorreo();
+		validatePassword();
+		validateOpciones();
+		mensaje.innerHTML = "";
+	}
+	
+}
+//mayuscula
+function convertirMayu(texto){
+	var newArray = texto.split("");
+	var primerLetra = newArray[0];
+	var mayuscula = primerLetra.toUpperCase();
+	var espacio = false;
+	for (var i=1; i<newArray.length; i++){
+        if(espacio){
+            mayuscula += newArray[i].toUpperCase();
+            espacio =false;
+        }else
+		mayuscula += newArray[i];
+		if(newArray[i] ==" ")
+		espacio = true;
+					
+		}
+	return mayuscula;
+}
+//creando span
+function crearSpan(space, valor ){
 	var span = document.createElement("span");
 	span.appendChild(valor);
 	var div = space.parentNode;
 	var salida = div.appendChild(span);
 	//return div;
  }
-
-function validacion1() {
-    var nombre = document.getElementById("name");
-	var texto = document.createTextNode("Ingrese su nombre");
-	
-    
-	if(nombre.value == ""){
-		alerta(nombre, texto);
-	}
-	else{ 
-		var nameArray = nombre.value.split("");
-		var primeraLetra = nameArray[0];
-		var mayuscula = primeraLetra.toUpperCase();
-		var espacio = false;
-
-		for ( var i = 1; i<nameArray.length; i++){
-			if(espacio){
-			mayuscula += nameArray[i].toUpperCase();
-			espacio = false;
-			}
-			else
-			mayuscula += nameArray[i];
-			if(nameArray[i] == "")
-			espacio = true;	
-		}
-	    document.getElementById("name").value = mayuscula;
-		nombre.parentNode.removeChild(nombre.nextSibling);
+//campo para eleminar
+function eliminar(campo){
+	var elemento = document.getElementById(campo);
+	if(elemento.nextSibling != null){
+		elemento.parentNode.removeChild(elemento.nextSibling);
 	}
 }
 
-function validacion2(_evt){
-	var letra = window.event.keyCode;
-	if((letra>=65 && letra<=90) || letra==8 || letra==32){	
+//creando mensaje en el span
+function mensaje(campo, texto){
+	var elemento = document.getElementById(campo);
+	//no existe
+	if(elemento.nextSibling == null){
+		crearSpan(elemento, texto);
 	}else{
-		_evt.preventDefault();
+		if(elemento.nextSibling.tagName == "span"){
+			elemento.nextSibling.innerHTML = texto;
+		}else{
+			elemento.parentNode.removeChild(elemento.nextSibling);
+			crearSpan(elemento, texto);
+		}
 	}
+}
+
+function validacion1() {
+    var nombre = document.getElementById("name");
+	var msm = document.createTextNode("Ingrese su nombre");
+	var number = document.createTextNode("No permite numeros");
+	if(nombre.value == ""){
+		mensaje("name", msm);
+	}if(nombre.value != ""){
+		eliminar("name");
+	}if(/([0-9])/g.test(nombre.value)){
+		mensaje("name", number);
+	}if(nombre.value != ""){
+		nombre.value = convertirMayu(nombre.value);
+	}else{
+		mensaje("name", msm);
+	}
+    
 }
 
 function validacionApellido(){
 	
     var apellido = document.getElementById("lastname");
-	var apell = document.createTextNode("Campo Apellido requerido");
+	var textApell = document.createTextNode("Campo Apellido requerido");
+	var noNumber = document.createTextNode("Solo acepta letra");
     if(apellido.value == ""){
-		alerta(apellido, apell);
-	} 
-	else{ 
-    var nameArray= apellido.value.split("");
-    var primeraLetra= nameArray[0];
-    var mayuscula = primeraLetra.toUpperCase();
-    var espacio= false;
-    
-    for (var i=1; i<nameArray.length; i++){
-        if(espacio){
-            mayuscula += nameArray[i].toUpperCase();
-            espacio =false;
-        }else
-		mayuscula += nameArray[i];
-		if(nameArray[i] ==" ")
-		espacio = true;
-					
-		}
-		document.getElementById("lastname").value = mayuscula;
-		apellido.parentNode.removeChild(apellido.nextSibling);
+		mensaje("lastname", textApell);
 	}
-}
-
-function validacionLastname(_evt){
-	var letra = window.event.keyCode;
-	
-	if((letra>=65 && letra<=90) || letra==8 || letra==32){	
-	}else{
-		_evt.preventDefault();
+	else{
+		eliminar("lastname")
+	}
+	if(/([0-9])/g.test(apellido.value)){
+		mensaje("lastname", noNumber);
+	}if(apellido != ""){
+		apellido.value = convertirMayu(apellido.value);
 	}
 }
 
 // validacion de todos los campos requeridos
-function validar(){
+function validateCorreo(){
 	
 // section correo 
 	var email = document.getElementById("input-email");
@@ -90,38 +112,42 @@ function validar(){
 	var salida3 = document.createTextNode("correo invalido");
 	var expresion = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 	if(email.value == ""){
-		alerta(email, correo);
-	}else if(!expresion.test(email.value)){
-		alerta(email, salida3);
+		mensaje("input-email", correo);
+	}
+	else if(!expresion.test(email.value)){
+		mensaje("input-email", salida3);
 	}
 	else{
-		
-		email.parentNode.removeChild(email.nextSibling);
+		eliminar("input-email");
 	}
+}
 // section password
-	var pass = document.getElementById("input-password");
+function validatePassword(_evt){ 
+	var password = document.getElementById("input-password");
 	var passText = document.createTextNode("Contraseña invalida");
 	var clave = document.createTextNode("Ingresa contraseña");
 	var salida4 = document.createTextNode("Minimo 6 caracteres");
-	if(pass.value === "123456"||pass.value === "098754"||pass.value === "password"){
-		alerta(pass, passText);
+	if(password.value === "123456"||password.value === "098754"||password.value === "password"){
+		mensaje("input-password", passText);
 	}
-	else if(pass.value == ""){
-		alerta(pass, clave);
+	else if(password.value === ""){
+		mensaje("input-password", clave);
 	}
-	else if(pass.value.length<6){
-		alerta(pass, salida4);
+	else if(password.value.length<6){
+		mensaje("input-password", salida4);
 	}else{
 		
-		pass.parentNode.removeChild(pass.nextSibling);
+		eliminar("input-password")
 	}
+}
 // section selector
+function validateOpciones(){ 
 	var indice = document.getElementById("opciones");
 	var salida5 = document.createTextNode("No seleccionaste ninguna opcion!");
 	if(indice.value == "0"||indice.value == null){
-		alerta(indice, salida5);
+		mensaje("opciones", salida5);
 	}else{
-		indice.parentNode.removeChild(indice.nextSibling);
+		eliminar("opciones")
 	}
 	
 	
